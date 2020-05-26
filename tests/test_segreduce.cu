3d3eb3b72b7bdc8a4a399a2c7796823a15ec6e39
Achal Agarwal <achalagarwal.01@gmail.com>
2020-05-25 19:57:11 -0700

    Ok, segreduce finally works now

--- tests/test_segreduce.cu
+++ tests/test_segreduce.cu
@@ -23,10 +23,7 @@ void test_segreduce(int count, int num_segments, int seed,
     // printf("Segment length at %d : %d\n", i, segments_host[i]);
   }
   std::sort(segments_host.begin() + 1, segments_host.end());
-  for(int i = 1; i < num_segments; ++i){
-    // segments_host[i] = dist(mt19937);
-    printf("Segment length at %d : %d\n", i, segments_host[i]);
-  }
+
   mem_t<int> segments = to_mem(segments_host, context);
   mem_t<quad> results(num_segments, context);
 
@@ -40,15 +37,18 @@ void test_segreduce(int count, int num_segments, int seed,
       j_end = segments_host[i+1];
     }
     int counter = 0;
-    int length = j_end - j;
     int store=1;
+    int length = j_end - j;
+    // give a center element the longest length
+    // length/4, length/2, length/4
     for(;j<j_end;j++){
       ++counter;
-      data_host[j] = store;
-      if(counter>=10+store){
-        store+=1;
+      data_host[j] = store==3?2:store;
+      if(counter>=length/4){
+       store+=1;
         counter = 0;
     }
+    // else if(counter)
    
       // create a sorted list in this loop
       // if(dist(mt19937)%2 == 0){
@@ -60,9 +60,9 @@ void test_segreduce(int count, int num_segments, int seed,
       
     }
   }
-  for(int i = segments_host[89];i<segments_host[90];i++){
-    printf("Value: %d \n", data_host[i]);
-  }
+  // for(int i = segments_host[89];i<segments_host[90];i++){
+  //   printf("Value: %d \n", data_host[i]);
+  // }
   mem_t<int> data = to_mem(data_host, context);
 
   quad init = {-1,0,-1,0,-1,0,-1};
@@ -81,7 +81,7 @@ void test_segreduce(int count, int num_segments, int seed,
   // Retrieve the results and verify them.
   std::vector<quad> results_host = from_mem(results);
   for(int i = 0;i<num_segments-1;i++){
-    printf("The segment length at i:%d is %d and output received is %d with count: %d \n", i, segments_host[i+1]-segments_host[i], results_host[i].best_element, results_host[i].best_count);
+    // printf("The segment length at i:%d is %d and output received is %d with count: %d \n", i, segments_host[i+1]-segments_host[i], results_host[i].best_element, results_host[i].best_count);
   }
   // for(int seg = 0; seg < num_segments; ++seg) {
   //   int begin = segments_host[seg];
@@ -111,8 +111,8 @@ int main(int argc, char** argv) {
   standard_context_t context;
 
   for(int test = 0; test < 1; ++test) {
-    int count =   100000;
-    int seg_size =  100;
+    int count =   100000000;
+    int seg_size =  1000000;
     test_segreduce(count, div_up(count, seg_size), test, context);
   }
 
